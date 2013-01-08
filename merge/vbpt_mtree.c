1d78b013bca3b98abe34d4bfd1bcb3d5a469b470
Kornilios Kourtis <kkourt@inf.ethz.ch>
2013-01-08 19:03:14 +0100

    vbpt_mtree: split it into a .c and .h file + updates

---
+++ merge/vbpt_mtree.c
@@ -0,0 +1,94 @@
+#include "vbpt.h"
+#include "vbpt_mtree.h"
+
+/*  mutable tree objects on top of immutable versioned trees */
+
+/**
+ * allocate a new mtree and initialize it with @tree
+ *
+ * return the mtree
+ *
+ * @tree's refcount is not increased
+ */
+vbpt_mtree_t *
+vbpt_mtree_alloc(vbpt_tree_t *tree)
+{
+	vbpt_mtree_t *ret = xmalloc(sizeof(vbpt_mtree_t));
+	ret->mt_tree = tree;
+	spinlock_init(&ret->mt_lock);
+	spinlock_init(&ret->gc_lock);
+	ver_pin(ret->mt_tree->ver, NULL);
+	return ret;
+}
+
+
+void
+vbpt_mtree_dealloc(vbpt_mtree_t *mtree, vbpt_tree_t **tree_ptr)
+{
+	vbpt_tree_t *tree = mtree->mt_tree;
+
+	free(mtree);
+
+	if (tree_ptr) {
+		*tree_ptr = tree;
+	} else {
+		ver_t *v = tree->ver;
+		vbpt_tree_dealloc(tree);
+		ver_tree_gc(v);
+		if (v->parent != NULL) {
+			ver_chain_print(v);
+			exit(1);
+		}
+		ver_unpin(v);
+	}
+}
+
+/**
+ * try to commit a new version to @mtree
+ *
+ * @tree:        the new version of the tree
+ * @b_ver:       the version @tree is based on
+ * @mt_tree_dst: if not NULL, a copy of old version of the tree is placed here
+ *
+ * if (un)successful true (false) is returned.
+ *
+ * @tree's refcount is not increased
+ * if @mt_tree_dst is not NULL, caller is responisble for releasing the tree
+ * using vbpt_tree_dealloc().
+ */
+bool
+vbpt_mtree_try_commit(vbpt_mtree_t *mtree, vbpt_tree_t *tree,
+                      ver_t *b_ver, vbpt_tree_t *mt_tree_dst)
+{
+	VBPT_START_TIMER(mtree_try_commit);
+	bool committed = false;
+	vbpt_tree_t *mt_tree;
+	spin_lock(&mtree->mt_lock);
+	ver_t *cur_ver = (mt_tree = mtree->mt_tree)->ver;
+	//tmsg("trying to commit ver:%zd to cur_ver:%zd\n", tree->ver->v_id, cur_ver->v_id);
+	if (ver_eq(cur_ver, b_ver)) {
+		//tmsg("commited ver:%zd to previous:%zd\n", tree->ver->v_id, cur_ver->v_id);
+		mtree->mt_tree = tree;
+		committed = true;
+	}
+	if (mt_tree_dst)
+		vbpt_tree_copy(mt_tree_dst, mt_tree);
+	spin_unlock(&mtree->mt_lock);
+
+	// pin without holding the lock
+	if (committed) {
+		ver_pin(mtree->mt_tree->ver, mt_tree->ver);
+
+		// run gc for versions before the pinned version
+		// if somebody else has the lock, just continue
+		if (spin_try_lock(&mtree->gc_lock)) {
+			ver_tree_gc(mtree->mt_tree->ver);
+			spin_unlock(&mtree->gc_lock);
+		}
+
+		vbpt_tree_dealloc(mt_tree);
+	}
+	VBPT_STOP_TIMER(mtree_try_commit);
+
+	return committed;
+}
--- merge/vbpt_mtree.h
+++ merge/vbpt_mtree.h
@@ -22,7 +22,8 @@
 /**
  * Mutable trees:
  * @tree_current current tree version
- * @mt_lock      atomically update mtree
+ * @mt_lock  atomically update mtree
+ * @gc_lock  serializing version gc
  */
 struct vbpt_mtree {
 	vbpt_tree_t *mt_tree;
@@ -31,40 +32,8 @@ struct vbpt_mtree {
 };
 typedef struct vbpt_mtree vbpt_mtree_t;
 
-/**
- * allocate a new mtree and initialize it with @tree
- *
- * return the mtree
- *
- * @tree's refcount is not increased
- */
-static inline vbpt_mtree_t *
-vbpt_mtree_alloc(vbpt_tree_t *tree)
-{
-	vbpt_mtree_t *ret = xmalloc(sizeof(vbpt_mtree_t));
-	ret->mt_tree = tree;
-	spinlock_init(&ret->mt_lock);
-	spinlock_init(&ret->gc_lock);
-	ver_pin(ret->mt_tree->ver, NULL);
-	return ret;
-}
-
-static inline void
-vbpt_mtree_dealloc(vbpt_mtree_t *mtree, vbpt_tree_t **tree_ptr)
-{
-	vbpt_tree_t *tree = mtree->mt_tree;
-
-	free(mtree);
-
-	if (tree_ptr) {
-		*tree_ptr = tree;
-	} else {
-		ver_t *v = tree->ver;
-		vbpt_tree_dealloc(tree);
-		ver_tree_gc(v);
-		ver_unpin(v);
-	}
-}
+vbpt_mtree_t *vbpt_mtree_alloc(vbpt_tree_t *tree);
+void          vbpt_mtree_dealloc(vbpt_mtree_t *mtree, vbpt_tree_t **tree_ptr);
 
 /* we do a branch (i.e., grab a references for the root and version) under a
  * lock, so that it won't dissapear */
@@ -76,46 +45,10 @@ vbpt_mtree_branch(vbpt_mtree_t *mtree, vbpt_tree_t *tree)
 	spin_unlock(&mtree->mt_lock);
 }
 
-/**
- * try to commit a new version to @mtree
- *
- * @tree:        the new version of the tree
- * @b_ver:       the version @tree is based on
- * @mt_tree_dst: if not NULL, a copy of old version of the tree is placed here
- *
- * if (un)successful true (false) is returned.
- *
- * @tree's refcount is not increased
- * if @mt_tree_dst is not NULL, caller is responisble for releasing the tree
- * using vbpt_tree_dealloc().
- */
-static inline bool
-vbpt_mtree_try_commit(vbpt_mtree_t *mtree, vbpt_tree_t *tree,
-                      ver_t *b_ver, vbpt_tree_t *mt_tree_dst)
-{
-	bool committed = false;
-	vbpt_tree_t *mt_tree;
-	spin_lock(&mtree->mt_lock);
-	ver_t *cur_ver = (mt_tree = mtree->mt_tree)->ver;
-	//tmsg("trying to commit ver:%zd to cur_ver:%zd\n", tree->ver->v_id, cur_ver->v_id);
-	if (ver_eq(cur_ver, b_ver)) {
-		//tmsg("commited ver:%zd to previous:%zd\n", tree->ver->v_id, cur_ver->v_id);
-		mtree->mt_tree = tree;
-		committed = true;
-	}
-	if (mt_tree_dst)
-		vbpt_tree_copy(mt_tree_dst, mt_tree);
-	spin_unlock(&mtree->mt_lock);
 
-	// pin without holding the lock
-	if (committed) {
-		ver_pin(mtree->mt_tree->ver, mt_tree->ver);
-		VBPT_START_TIMER(mtree_try_commit);
-		vbpt_tree_dealloc(mt_tree);
-		VBPT_STOP_TIMER(mtree_try_commit);
-	}
+bool vbpt_mtree_try_commit(vbpt_mtree_t *mtree,
+                           vbpt_tree_t *tree, ver_t *b_ver,
+                           vbpt_tree_t *mt_tree_dst);
 
-	return committed;
-}
 
 #endif /* VBPT_MTREE_H */
